5e6d25faa99029dffc7c9bfc694c036871f129d3
juryanyingyu <765897439@qq.com>
2018-05-21 23:49:25 +0800

    20180521

---
+++ AI.c
@@ -0,0 +1,16 @@
+#include "ShapeType.h"
+
+int shapejudge(int tox,int toy) {
+	if(metacount(1,0)) {
+		
+    }
+	if(metacount(1,1)) {
+		
+    }
+	if(metacount(0,1)) {
+		
+    }
+	if(metacount(-1,1)) {
+		
+    }
+}
--- Gamesupport.h
+++ Gamesupport.h
@@ -5,8 +5,6 @@
 #define N 15
 #define MAXSTEPS 225
 #define DRAW 0
-#define WHITE_WIN 1
-#define BLACK_WIN 2
 #define TRUE 1
 #define FALSE 0
 #define WHITE -1
@@ -14,7 +12,7 @@
 #define NONE 0
 
 int n[N][N]={0};
-int WorB=WHITE;
+int WorB=BLACK;
 int x=-1,y=-1;
 int lastx,lasty;
 int steps=0;
@@ -24,4 +22,6 @@ int winjudge();
 int metajudge(int dx,int dy);
 int down();
 void cheki();
-
+void ai();
+int shapejudge(int,int);
+int metacount(int dx,int dy);
